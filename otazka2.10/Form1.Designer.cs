0af0b5a6223ac35b80cd5c530cf37a8e6fb509e0
Filip Iran <72192710+F1lgas@users.noreply.github.com>
2022-11-24 11:07:00 +0100

    v1.0

---
+++ otazka2.10/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace otazka2._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(138, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Součet cifer";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(138, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(109, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Obsahuje 3 velká";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 140);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ otazka2.10/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otazka2._10
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                int n = Convert.ToInt32(textBox1.Text);
+
+                for (int i = 0; i < n; i++)
+                {
+                    char znak = Convert.ToChar(rnd.Next(36) + 42);
+
+                    listBox1.Items.Add(znak);
+                }
+
+                if (checkBox1.Checked == true)
+                {
+                    int soucet = 0;
+
+                    for (int y = 0; y < listBox1.Items.Count; y++)
+                    {
+                        if (Convert.ToChar(listBox1.Items[y]) >= '0' && Convert.ToChar(listBox1.Items[y]) <= '9')
+                        {
+                            soucet += Convert.ToInt32(listBox1.Items[y]) - 48;
+                        }
+                    }
+                    if (soucet > 0)
+                    {
+                        MessageBox.Show("Součet všech cifer v listBoxu je " + soucet);
+                    }
+                    else
+                    {
+                        MessageBox.Show("listBox neobsahuje žádné cifry!");
+                    }
+                }
+                if (checkBox2.Checked == true)
+                {
+                    int pocet = 0;
+                    bool konec = false;
+
+                    for (int y = 0; y < listBox1.Items.Count; y++)
+                    {
+                        if (Convert.ToChar(listBox1.Items[y]) >= 'A' && Convert.ToChar(listBox1.Items[y]) <= 'M')
+                        {
+                            pocet++;
+                        }
+
+                        if (pocet >= 3)
+                        {
+                            konec = true;
+                        }
+
+                        if (konec == true)
+                        {
+                            break;
+                        }
+                    }
+                    if (konec == false)
+                    {
+                        MessageBox.Show("listBox neobsahuje 3 velká písmena!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("listBox obsahuje 3 velká písmena");
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Nezadal jsi číslo!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nezadal jsi žádné číslo!");
+            }
+        }
+    }
+}
